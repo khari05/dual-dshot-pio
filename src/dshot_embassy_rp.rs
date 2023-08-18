93ec1586ad18672fbf810e9ae7b0d47061246095
Kali Hari <35894769+khari05@users.noreply.github.com>
2023-08-18 00:21:07 -0400

    remove minimal amounts of code for my purposes

--- src/dshot_embassy_rp.rs
+++ src/dshot_embassy_rp.rs
@@ -1,23 +1,24 @@
 use dshot_encoder as dshot;
-pub use super::QuadDshotTrait;
+pub use super::DualDshotTrait;
 
 use embassy_rp::{
-    pio::{ Instance, Pio, Config, PioPin, ShiftConfig, ShiftDirection::Left},
+    pio::{ Instance, Pio, Config, PioPin, ShiftConfig, ShiftDirection::Left, InterruptHandler},
     relocate::RelocatedProgram,
-    Peripheral
+    Peripheral, interrupt::typelevel::Binding
 };
 #[allow(dead_code)]
-pub struct QuadDshotPio<'a,PIO : Instance> {
+pub struct DualDshotPio<'a,PIO : Instance> {
     pio_instance: Pio<'a,PIO>
 }
 
 #[allow(dead_code)]
-impl <'a,PIO: Instance> QuadDshotPio<'a,PIO> {
+impl <'a,PIO: Instance> DualDshotPio<'a,PIO> {
     pub fn new(
         pio: impl Peripheral<P = PIO> + 'a,
-        pin0: impl PioPin, pin1: impl PioPin, pin2: impl PioPin, pin3: impl PioPin,
+        pin0: impl PioPin, pin1: impl PioPin,
         clk_div: (u16, u8),
-    ) -> QuadDshotPio<'a,PIO> {
+        _irq: impl Binding<PIO::Interrupt, InterruptHandler<PIO>>
+    ) -> DualDshotPio<'a,PIO> {
 
         // Define program
 
@@ -52,7 +53,7 @@ impl <'a,PIO: Instance> QuadDshotPio<'a,PIO> {
         // Configure state machines
 
         let mut cfg = Config::default();
-        let mut pio = Pio::new(pio);
+        let mut pio = Pio::new(pio, _irq);
         cfg.use_program(&pio.common.load_program(&relocated), &[]);
         cfg.clock_divider = clk_div.0.into();
 
@@ -80,47 +81,31 @@ impl <'a,PIO: Instance> QuadDshotPio<'a,PIO> {
         pio.sm1.set_config(&cfg);
         pio.sm1.set_enable(true);
 
-        let pin2 = pio.common.make_pio_pin(pin2);
-        cfg.set_set_pins(&[&pin2]);
-        pio.sm2.set_config(&cfg);
-        pio.sm2.set_enable(true);
-
-        let pin3 = pio.common.make_pio_pin(pin3);
-        cfg.set_set_pins(&[&pin3]);
-        pio.sm3.set_config(&cfg);
-        pio.sm3.set_enable(true);
-
         // Return struct of four configured DSHOT state machines
-        QuadDshotPio { pio_instance : pio }
+        DualDshotPio { pio_instance : pio }
     }
 }
 
-impl <'d,PIO : Instance> super::QuadDshotTrait for QuadDshotPio<'d,PIO> {
+impl <'d,PIO : Instance> super::DualDshotTrait for DualDshotPio<'d,PIO> {
 
     /// Set the direction of rotation for each motor
-    fn reverse(&mut self, reverse: (bool, bool, bool, bool)) -> (bool, bool, bool, bool) {
+    fn reverse(&mut self, reverse: (bool, bool)) -> (bool, bool) {
         self.pio_instance.sm0.tx().push(dshot::reverse(reverse.0) as u32);
         self.pio_instance.sm1.tx().push(dshot::reverse(reverse.1) as u32);
-        self.pio_instance.sm2.tx().push(dshot::reverse(reverse.2) as u32);
-        self.pio_instance.sm3.tx().push(dshot::reverse(reverse.3) as u32);
-        (true,true,true,true) // Embassy PIO does not report failure to push to TX buffer
+        (true,true) // Embassy PIO does not report failure to push to TX buffer
     }
 
     /// Set the throttle for each motor. All values are clamped between 48 and 2047
-    fn throttle_clamp(&mut self, throttle: (u16, u16, u16, u16)) -> (bool, bool, bool, bool) {
+    fn throttle_clamp(&mut self, throttle: (u16, u16)) -> (bool, bool) {
         self.pio_instance.sm0.tx().push(dshot::throttle_clamp(throttle.0, false) as u32);
         self.pio_instance.sm1.tx().push(dshot::throttle_clamp(throttle.1, false) as u32);
-        self.pio_instance.sm2.tx().push(dshot::throttle_clamp(throttle.2, false) as u32);
-        self.pio_instance.sm3.tx().push(dshot::throttle_clamp(throttle.3, false) as u32);
-        (true,true,true,true) // Embassy PIO does not report failure to push to TX buffer
+        (true,true) // Embassy PIO does not report failure to push to TX buffer
     }
 
     /// Set the throttle for each motor to zero (Dshot command 48)
-    fn throttle_minimum(&mut self) -> (bool, bool, bool, bool) {
+    fn throttle_minimum(&mut self) -> (bool, bool) {
         self.pio_instance.sm0.tx().push(dshot::throttle_minimum(false) as u32);
         self.pio_instance.sm1.tx().push(dshot::throttle_minimum(false) as u32);
-        self.pio_instance.sm2.tx().push(dshot::throttle_minimum(false) as u32);
-        self.pio_instance.sm3.tx().push(dshot::throttle_minimum(false) as u32);
-        (true,true,true,true) // Embassy PIO does not report failure to push to TX buffer
+        (true,true) // Embassy PIO does not report failure to push to TX buffer
     }
 }
